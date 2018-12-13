d36a50db61ed1c9cd82e84ba0f800262b7ed9fa9
Christopher Moore <christopherkmoore1@gmail.com>
2018-12-12 22:55:41 -0800

    reoganises structure

---
+++ wine_map_app/Controllers/WineMapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WineMapViewController.swift
+//  wine_map_app
+//
+//  Created by Christopher Moore on 12/12/18.
+//  Copyright © 2018 Christopher Moore. All rights reserved.
+//
+
+import Foundation
