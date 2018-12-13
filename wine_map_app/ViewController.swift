bb5b7dc764c4c513f120f6a15eed23155e637581
Christopher Moore <christopherkmoore1@gmail.com>
2018-12-12 22:02:42 -0800

    Initial Commit

---
+++ wine_map_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wine_map_app
+//
+//  Created by Christopher Moore on 12/12/18.
+//  Copyright © 2018 Christopher Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
